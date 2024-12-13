95ebbf08a856d568291ec257d4136efdb3abaa9c
KARBOU Aristide <aristidetiger12@gmail.com>
2024-12-13 12:17:28 +0000

    feat: Add dark mode toggle to dashboard layout - Integrate ThemeProvider to enable system-level theme switching - Add ModeToggle component to dashboard header - Ensure consistent styling and behavior across light and dark modes

--- src/app/(board)/layout.tsx
+++ src/app/(board)/layout.tsx
@@ -1,5 +1,6 @@
 import { AppSidebar } from "@/components/shared/board/app-sidebar";
 import { DynamicBreadcrumb } from "@/components/shared/board/breadcrumb";
+import { ModeToggle } from "@/components/shared/mode-toggle";
 import { Separator } from "@/components/ui/separator";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { Metadata } from "next";
@@ -21,6 +22,7 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
+            <ModeToggle />
             <Separator orientation="vertical" className="mr-2 h-4" />
             <DynamicBreadcrumb />
           </div>

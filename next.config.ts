be7a67f914c8806e3fdd0a270619e6f4ce03c6a2
KARBOU Aristide <aristidetiger12@gmail.com>
2024-12-16 11:30:35 +0000

    feat: Enhance image handling configuration - Increase minimum cache TTL for images to 300 seconds - Update device and image sizes for responsive image handling - Enable WebP image format support

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -7,13 +8,13 @@ const nextConfig: NextConfig = {
       { hostname: "avatars.githubusercontent.com" },
       { hostname: "minio.100visagesls.xyz" },
     ],
-
     //TODO: Check if this is needed and if it is the good config
-    minimumCacheTTL: 60,
+
+    minimumCacheTTL: 300,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ["image/webp"],
-    domains: ["minio.100visagesls.xyz"],
   },
 };
+
 export default nextConfig;

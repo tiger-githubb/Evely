26e03bdb5550e4818379a3d4b7e83bfa01656c11
KARBOU Aristide <aristidetiger12@gmail.com>
2024-12-19 14:04:38 +0000

    feat: Refactor event and organization pages to use async/await - Update the `EventPage` and `OrganizationPage` components to use async/await for fetching data - Simplify the parameter destructuring in the page components - Adjust the `EventPage` component to remove the `py-16 mt-0` class and use `md:mt-0` instead

--- src/app/(front)/e/[slug]/page.tsx
+++ src/app/(front)/e/[slug]/page.tsx
@@ -1,12 +1,17 @@
 import Section from "@/components/ui/custom/section";
 import { fetchEventBySlug } from "@/config/data";
 import { EventDetails } from "./_components/event-details";
-
-export default function EventPage({ params }: { params: { slug: string } }) {
-  const event = fetchEventBySlug(params.slug);
+interface EventsPageProps {
+  params: Promise<{
+    slug: string;
+  }>;
+}
+export default async function EventPage({ params }: EventsPageProps) {
+  const { slug } = await params;
+  const event = fetchEventBySlug(slug);
 
   return (
-    <Section className="py-16 mt-0">
+    <Section className="md:mt-0">
       <EventDetails event={event} />
     </Section>
   );
--- src/app/(front)/o/[slug]/page.tsx
+++ src/app/(front)/o/[slug]/page.tsx
@@ -2,8 +2,15 @@ import { fetchOrganizationBySlug } from "@/config/data";
 import { OrganizationContent } from "./_components/organization-content";
 import { OrganizationHeader } from "./_components/organization-header";
 
-export default function OrganizationPage({ params }: { params: { slug: string } }) {
-  const organization = fetchOrganizationBySlug(params.slug);
+interface OrganizationPageProps {
+  params: Promise<{
+    slug: string;
+  }>;
+}
+
+export default async function OrganizationPage({ params }: OrganizationPageProps) {
+  const { slug } = await params;
+  const organization = fetchOrganizationBySlug(slug);
 
   return (
     <div className="min-h-screen bg-muted/30">

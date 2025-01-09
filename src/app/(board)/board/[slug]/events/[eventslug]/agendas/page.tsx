8ad6a65f0741d5636c9214d502f578511d95542b
KARBOU Aristide <aristidetiger12@gmail.com>
2025-01-09 15:30:01 +0000

    feat: Update event agendas page props

--- src/app/(board)/board/[slug]/events/[eventslug]/agendas/page.tsx
+++ src/app/(board)/board/[slug]/events/[eventslug]/agendas/page.tsx
@@ -1,14 +1,15 @@
 import { fetchEvent } from "@/server/services/events.service";
 import EventAgendas from "./_components/event-agendas";
 
-interface AgendasPageProps {
+type PageProps = {
   params: {
     slug: string;
     eventslug: string;
   };
-}
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
-export default async function AgendasPage({ params }: AgendasPageProps) {
+export default async function AgendasPage({ params }: PageProps) {
   const { eventslug } = params;
 
   const eventResponse = await fetchEvent(eventslug);

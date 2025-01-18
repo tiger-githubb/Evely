e3b1b699963236c1935f00b0ca629a011e660a5e
KARBOU Aristide <aristidetiger12@gmail.com>
2025-01-18 15:34:47 +0000

    feat: add hero section and event table for TTD page
    - Add `HeroSection` component with gradient animation
    - Add `TTDPage` component with `HeroSection` and `EventTable`
    - Update `tailwind.config.ts` with new gradient animation keyframes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -65,24 +65,28 @@ export default {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
-        "accordion-down": {
-          from: {
-            height: "0",
+        "gradient-x": {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
           },
-          to: {
-            height: "var(--radix-accordion-content-height)",
+
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
           },
         },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
         "accordion-up": {
-          from: {
-            height: "var(--radix-accordion-content-height)",
-          },
-          to: {
-            height: "0",
-          },
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
         },
       },
       animation: {
+        "gradient-x": "gradient-x 8s ease infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },

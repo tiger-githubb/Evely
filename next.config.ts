63a8e1ddc4d0cd60da875a8178c3e0f2fb0fa9c4
KARBOU Aristide <aristidetiger12@gmail.com>
2025-01-20 22:57:14 +0000

    feat: configure Docker and Next.js for production
    - Add Dockerfile for building and running the application in a Docker container
    - Set `output: "standalone"` in Next.js configuration to optimize for production
    - Update image configuration to use remote patterns and minimum cache TTL
    - Remove unnecessary TODO comment

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,13 @@ import type { NextConfig } from "next";
 import withNextIntl from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
+  output: process.platform === "win32" ? undefined : "standalone",
   images: {
     remotePatterns: [{ hostname: "yala.events" }, { hostname: "storage.yala.events" }],
-
-    //TODO: Check if this is needed and if it is the good config
     minimumCacheTTL: 300,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ["image/webp"],
   },
 };
-
 export default withNextIntl()(nextConfig);
